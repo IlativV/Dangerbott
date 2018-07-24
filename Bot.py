ec118e341b4a558542504c148d01966a7601086a
IlativV <39774514+IlativV@users.noreply.github.com>
2018-07-24 11:28:51 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -21,4 +21,4 @@ async def on_member_join(member):
     role = discord.utils.get(member.server.roles, id="397010173513826306")
     await client.add_roles(member, role)
 
-client.run('NDU5MDc2MjM1MzY5MTE5NzQ0.Dgw7og.aMKdT8tUYG-s5OIkyX9hwzqxxSs')
+client.run(os.getenv('TOKEN'))
