eb7c91f9e906f934aa7e007696ca4ef3a3035110
IlativV <39774514+IlativV@users.noreply.github.com>
2018-07-24 14:34:40 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -37,7 +37,7 @@ async def on_message(message):
     if message.content.lower().startswith('!facebook'):
         await client.send_message(message.channel, "Hier ist meine Facebook Seite: https://goo.gl/35VuhR")
     if message.content.lower().startswith('!premium'):
-        await client.send_message(message.channel, "Wenn du diesen Command "/geworben Y8DEEH70" auf dem UnicaCity Server eingibst, bekommst du 14 Tage Premium (auf UnicaCity) nachdem du Level 10 auf dem Server erreicht hast.")      
+        await client.send_message(message.channel, "Wenn du diesen Command '/geworben Y8DEEH70' auf dem UnicaCity Server eingibst, bekommst du 14 Tage Premium (auf UnicaCity) nachdem du Level 10 auf dem Server erreicht hast.")      
         
 @client.async_event
 async def on_member_join(member):
