6eb528274d4c08f58687dd210a1c6b2fda91cd9d
IlativV <39774514+IlativV@users.noreply.github.com>
2018-07-24 13:13:44 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,24 +7,21 @@ import os
 
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
     print(client.user.name)
     print('ist jetzt aktiv!')
     print('----------------')
-
-
+    
+    
 @client.event
 async def on_message(message):
     if message.content.lower().startswith('!abo'):
         await client.send_message(message.channel, "Noch kein Abonnent ? Hier kannst du mich abonnieren : https://goo.gl/kHwHVN")
-
-
 
 @client.async_event
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, id="397010173513826306")
     await client.add_roles(member, role)
 
-client.run(os.getenv('TOKEN')
+client.run(os.getenv('TOKEN'))
