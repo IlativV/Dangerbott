f14b7e568ece39bc8105f8124ef69c503f0b642c
IlativV <39774514+IlativV@users.noreply.github.com>
2018-08-03 17:10:51 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -18,7 +18,7 @@ async def on_ready():
 async def on_message(message):
 await client.send_message(message.channel, "bot")
     if message.content.lower().startswith('!youtube'):
-        await client.send_message(message.channel, "Hier gibt es Videos die dir gefallen könnten : https://goo.gl/8HUkBW")
+        await client.send_message(message.channel,  "bot" "Hier gibt es Videos die dir gefallen könnten : https://goo.gl/8HUkBW")
     if message.content.lower().startswith('!abo'):
         await client.send_message(message.channel, "Noch kein Abonnent ? Hier kannst du mich abonnieren: https://goo.gl/kHwHVN")
     if message.content.lower().startswith('!server'):
