48e2bc40d4c65ad53c27c69e2701eb52b65a6295
IlativV <39774514+IlativV@users.noreply.github.com>
2018-08-16 21:51:43 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -17,12 +17,8 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.lower().startswith('!youtube'):        
-        for role in message.author.roles:
-            if role.name == "Discord Mod":
-                await client.send_message(message.channel, "**Hier gibt es Videos die dir gefallen könnten : https://www.youtube.com/DangerZockt**")
         if message.channel.id == "474317552936288277":
-            await client.delete_message(message)
-            await client.send_message(message.channel, "Hier gibt es Videos die dir gefallen könnten : https://www.youtube.com/DangerZockt")  
+            await client.send_message(message.channel, "**Hier gibt es Videos die dir gefallen könnten : https://www.youtube.com/DangerZockt**")
         else:    
             await client.delete_message(message)
             await client.send_message(message.channel, "Commands bitte nur im <#474317552936288277> Channel eingeben!")     
