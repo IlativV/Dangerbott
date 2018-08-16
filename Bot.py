7c2c2e2906d404ba72939abbd947fcbf5ef33524
IlativV <39774514+IlativV@users.noreply.github.com>
2018-08-16 14:07:02 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -19,10 +19,10 @@ async def on_message(message):
     if message.content.lower().startswith('!youtube'):
         if message.channel.id == "474317552936288277":
             await client.send_message(message.channel, "Hier gibt es Videos die dir gefallen könnten : https://goo.gl/8HUkBW")
-        for role in message.author.roles:
+        elif role in message.author.roles:
             if role.name == "Discord Mod":                  
                 await client.send_message(message.channel, "Hier gibt es Videos die dir gefallen könnten : https://goo.gl/8HUkBW")  
-        if message.channel.id != "474317552936288277":    
+        else:    
             await client.send_message(message.channel, "Commands bitte nur im <#474317552936288277> Channel eingeben!")
 #        for role in message.author.roles:
 #            if role.name == "Discord Mod":             
@@ -45,12 +45,15 @@ async def on_message(message):
     if message.content.lower().startswith('!premium'):
         await client.send_message(message.channel, "Wenn du diesen Command `/geworben Y8DEEH70` auf dem UnicaCity Server eingibst, bekommst du 14 Tage Premium (auf UnicaCity) nachdem du Level 10 auf dem Server erreicht hast.")      
     if message.content.lower().startswith('!commands'):
-        await client.send_message(message.channel, "**Hier sind die Commands:**\n\n`!abo` zeigt dir wo du Danger abonnieren kannst.\n`!einladung` gibt dir eine einladung.\n`!facebook` gibt dir den Link zur Facebook Seite von Danger.\n`!premium` beschreibt dir wie du premium auf UnicaCity bekommst.\n`!server` zeigt dir den Server auf dem Danger zurzeit spielt.\n`!tp` zeigt dir das Resource Pack mit dem Danger zurzeit spielt.\n`!trinkgeld` zeigt dir einen Link wo du Danger etwas Trinkgeld hinterlassen kannst.\n`!twitch` gibt dir den Link zu Dangers Twitch Kanal.\n`!twitter` gibt dir den Link zur Twitter Seite von Danger.\n`!uploadplan` gibt dir den Link der dich zum Uploadplan von Danger weiterbringt.\n`!youtube` gibt dir den Link zu Dangers YouTube Kanal.\n`tippspiel` gibt dir den Link zum Tippspiel von Danger.")
+        await client.send_message(message.channel, "**Hier sind die Commands:**\n\n`!abo` zeigt dir wo du Danger abonnieren kannst.\n`!einladung` gibt dir eine einladung.\n`!facebook` gibt dir den Link zur Facebook Seite von Danger.\n`!premium` beschreibt dir wie du premium auf UnicaCity bekommst.\n`!server` zeigt dir den Server auf dem Danger zurzeit spielt.\n`!tp` zeigt dir das Resource Pack mit dem Danger zurzeit spielt.\n`!trinkgeld` zeigt dir einen Link wo du Danger etwas Trinkgeld hinterlassen kannst.\n`!twitch` gibt dir den Link zu Dangers Twitch Kanal.\n`!twitter` gibt dir den Link zur Twitter Seite von Danger.\n`!uploadplan` gibt dir den Link der dich zum Uploadplan von Danger weiterbringt.\n`!youtube` gibt dir den Link zu Dangers YouTube Kanal.\n`!tippspiel` gibt dir den Link zum Tippspiel von Danger.")
     if message.content.lower().startswith('!einladung'):
         await client.send_message(message.channel, "Hier ist ein Link womit du Freunde auf denn Danger Zockt Community Discord Server einladen kannst: https://discord.gg/qmzzm5y .")                                           
     if message.content.lower().startswith('!tippspiel'):
         await client.send_message(message.channel, "Bundesliga Tippspiel 2018/19 - trete der Dangerliga bei! https://dangerliga.webtippspiel.net")                                                
-        
+    if message.content.lower().startswith('!instagram'):
+        await client.send_message(message.channel, "Hier ist meine Instagramm Account: https://www.instagram.com/dangerzockt")
+    
+    
 @client.async_event
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, id="397010173513826306")
